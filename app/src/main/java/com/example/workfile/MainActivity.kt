53a9ed78c0af56730f42fa45e7ad4aa3dc10ab12
longsshen <s1082823@gm.pu.edu.tw>
2021-06-22 21:34:15 +0800

    龍新開專案按鈕設定taitung

--- app/src/main/java/com/example/workfile/MainActivity.kt
+++ app/src/main/java/com/example/workfile/MainActivity.kt
@@ -1,14 +1,24 @@
 package com.example.workfile
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        TaitungB.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(p0: View?) {
+                intent = Intent(this@MainActivity, QuestionActivity::class.java)
+                startActivity(intent)
+            }
+        })
+
     }
-
-
-
 }
