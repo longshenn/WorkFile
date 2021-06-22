0f8f3625bbb981bf41812af6fb996f6a5d769dd3
longsshen <s1082823@gm.pu.edu.tw>
2021-06-22 21:13:00 +0800

    龍新開專案按鈕設定

---
+++ app/src/main/java/com/example/workfile/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.workfile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+
+}
